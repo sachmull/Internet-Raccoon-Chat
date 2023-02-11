e03bb59ea7c421807d6c3d09720345552d0fe0eb
sachmull <sachmull@1-D-27.42heilbronn.de>
2023-02-11 18:15:46 +0100

    added: improved message parser

---
+++ inc/MsgParser.hpp
@@ -0,0 +1,64 @@
+#ifndef MSG_PARSER_HPP
+#define MSG_PARSER_HPP
+
+#include <iostream>
+
+#include <vector>
+#include <string>
+
+typedef enum e_command_type {
+	JOIN,
+	PASS,
+	NICK,
+	USER,
+	PRIVMSG,
+	OPER,
+	QUIT,
+	KICK,
+	MODE,
+	INVITE,
+	TOPIC,
+	UNKNOWN,
+}	t_command_type;
+
+class Prefix {
+	public:
+		std::string	name;
+		std::string	user;
+		std::string	host;
+};
+
+class Message {
+	public:
+		Prefix									prefix; //	prefix
+		t_command_type							command; //	command
+		std::vector<std::vector<std::string> >	params; //	params
+		bool									is_complete;	// allows if ((msg = parser.parse(input)).is_complete)
+
+	public:
+		Message();
+};
+
+class MsgParser {
+	private:
+		std::string				input;
+		std::string::size_type	pos;
+
+	public:
+		MsgParser();
+		Message	parse(std::string input);
+
+	private:
+		void	parse_prefix(Prefix& prefix);
+		void	parse_command(t_command_type& command_type);
+		void	parse_params(Message& msg);
+
+		void	skip_whitespace();
+};
+
+class Executor {
+	public:
+		void	execute(Message& msg);
+};
+
+#endif
